7b1d741565bb41a35a764a6fdac40fcd69c9a295
William Stevens <contact@wastevensv.com>
2017-03-15 22:04:13 -0400

    Added maximize option to sunrise program.

--- src/sunrise.cpp
+++ src/sunrise.cpp
@@ -1,10 +1,24 @@
 #include "display/display.h"
-#include <gtkmm/application.h>
-#include <gtkmm/window.h>
+#include <gtkmm.h>
 
 int main(int argc, char* argv[])
 {
-    auto app = Gtk::Application::create(argc, argv, "me.wasv.sunrise");
+
+    Glib::OptionContext options("Sunrise Alarm Clock");
+    options.set_help_enabled();
+
+    Glib::OptionGroup group("sunrise", "Sunrise");
+
+    bool maximize = false;
+    Glib::OptionEntry entry3;
+    entry3.set_long_name("maximize");
+    entry3.set_short_name('m');
+    entry3.set_description("Maximize the window.");
+    group.add_entry(entry3, maximize);
+
+    options.set_main_group(group);
+
+    Gtk::Main kit(argc, argv, options);
 
     Gtk::Window window;
     window.set_title("Sunrise");
@@ -13,5 +27,8 @@ int main(int argc, char* argv[])
     window.add(disp);
     disp.show();
 
-    return app->run(window);
+    if(maximize) window.fullscreen();
+
+    Gtk::Main::run(window);
+    return 0;
 }
